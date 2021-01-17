0c87739cfe7937be660069c606b78a4043b34097
ismairdrs <ismairdrs@outlook.com>
2021-01-17 13:36:40 -0300

    criando componente de lista e item

---
+++ frontend/src/ListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemComponent from './ItemComponent';
+
+export default function ListComponent() {
+    return (
+        <div>
+            <h2>Minha lista</h2>
+            <ul>
+                <ItemComponent name={'Meu Item'}/>
+            </ul>
+        </div>
+    )
+}
